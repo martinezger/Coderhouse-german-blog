a53b261a82c2b7d1f336cb098f23e1686a938017
Germán Martinez <germand_m@hotmail.com>
2022-05-26 23:52:58 -0300

    agrego timestamp automatico

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from operator import truediv
+from pyexpat import model
 from statistics import mode
 from django.db import models
 
@@ -6,3 +8,4 @@ class BlogModel(models.Model):
     sub_titulo = models.CharField(max_length=100)
     cuerpo = models.TextField()
     autor = models.CharField(max_length=100)
+    fecha_creacion = models.DateField(auto_now_add=True)
