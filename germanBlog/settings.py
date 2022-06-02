d11a3594d5ad5552f13b7abf5e212e9575d25c48
Germán Martinez <germand_m@hotmail.com>
2022-06-02 13:34:38 -0300

    mover a root blog

--- germanBlog/settings.py
+++ germanBlog/settings.py
@@ -10,11 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-<<<<<<< HEAD
-=======
 import dj_database_url
->>>>>>> 299b55a... Agrego button para deployar en Heroku
-from pathlib import Path
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
