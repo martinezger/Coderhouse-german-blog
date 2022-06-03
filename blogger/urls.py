eb408610db4bf6f688878278e66517729781b752
Germán Martinez <germand_m@hotmail.com>
2022-06-02 23:15:23 -0300

    Agregar avatar

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from blogger import views
 
@@ -6,3 +8,5 @@ urlpatterns = [
     path("profile/<pk>/", views.BloggerProfile.as_view(), name ="blogger_profile"),
     path("editar/<pk>/", views.BloggerUpdate.as_view(), name ="blogger_edit"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
