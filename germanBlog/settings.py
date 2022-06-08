87f2f705da97e68fd1831f05ec2f4b358ff75c8f
Germán Martinez <germand_m@hotmail.com>
2022-06-07 23:19:05 -0300

    arreglo el secret

--- germanBlog/settings.py
+++ germanBlog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get( "SECRET_KEY" , 'mysupersecretkey')
+SECRET_KEY = os.environ.get( "SECRET" , 'mysupersecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
